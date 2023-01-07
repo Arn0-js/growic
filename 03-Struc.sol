badcb06818f7246536e49113927bef2a9f072684
Arn0-js <92599854+Arn0-js@users.noreply.github.com>
2023-01-07 13:14:25 +0100

    Update 03-Struc.sol

--- 03-Struc.sol
+++ 03-Struc.sol
@@ -13,23 +13,26 @@ contract YourContract {
   mapping(address => uint256) public balances;
   mapping(address => userDetail) public userDetails;
 
+/// deposit function allowing user to populate their balance. 
   function deposit (uint256 amount) public payable {
      // require(msg.value = amount,"Please pay the amount"); not requested. 
       balances[msg.sender] += amount; // add the amount to the existing balance (0 being the default value, works too for empty balance)
       console.log(amount," added for ",msg.sender);
   }
 
+/// return the current balance of the user (sender)
   function checkBalance() view public returns(uint256){
     uint256 tempBal = balances[msg.sender];
     console.log("Check balance for ",msg.sender, ": ",tempBal);
     return tempBal; //balances[msg.sender]
   }
 
+/// function allowing user (sender) to set their personnal details 
   function setUserDetails(string calldata name, uint256 age) public{ 
    userDetails[msg.sender] = userDetail(name,age);
   }
 
-
+/// returns the personnal details of the user (sender)
   function getUserDetail() view public returns(string memory name, uint256 age){
    userDetail memory det=userDetails[msg.sender];
    return (det.name, det.age);
