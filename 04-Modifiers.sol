dc8ae9b7e0d84f3074c79140bc525e5925a715b4
Arn0-js <92599854+Arn0-js@users.noreply.github.com>
2023-01-10 00:17:36 +0100

    Update 04-Modifiers.sol

--- 04-Modifiers.sol
+++ 04-Modifiers.sol
@@ -1,4 +1,4 @@
-pragma solidity >=0.8.0 <0.9.0;
+ pragma solidity >=0.8.0 <0.9.0;
 //SPDX-License-Identifier: MIT
 
 import "hardhat/console.sol";
@@ -12,7 +12,7 @@ contract YourContract {
   
   mapping(address => uint256) public balances;
   mapping(address => userDetail) public userDetails;
-  uint256 private Fee;
+  uint256 private immutable Fee;
   address public owner = 0xDfDfDF033E7248df0770FC9f02a0DE5AeE625cf6;
   error unauthorized(); 
   error insufficientBalance();
@@ -24,12 +24,13 @@ contract YourContract {
  if(msg.value < _amount) revert insufficientAmount();
   _;
 }
+constructor() {
+  Fee = tx.gasprice;
+}
 
 /// deposit function storing the amount of deposited amount. 
   function deposit (uint256 amount) public payable amountCheck(amount) {
-    // require(msg.value == amount,"Please pay the amount"); // replaced by a custom error, personnal initiative
       balances[msg.sender] += amount; 
-      console.log(amount," added for ",msg.sender);
   }
 
  modifier existingDeposit(){
@@ -39,14 +40,13 @@ contract YourContract {
 }
 
 modifier checkFee(uint256 _amount){
-Fee = tx.gasprice;
-console.log("Fee comparison with the amount:",Fee," < ",_amount);
+// console.log("Fee comparison with the amount:",Fee," < ",_amount);
 if(_amount<Fee) revert amountTooSmall();
  _;
 }
 
 function addFund (uint256 amount) public payable existingDeposit() checkFee(amount){
-  balances[msg.sender] += amount; 
+  balances[msg.sender] += amount;
 }
 
 modifier ownerOnly{
@@ -58,13 +58,12 @@ function withdraw (uint256 amount) public ownerOnly {
   uint256 availableBal = balances[msg.sender]; 
   if(amount>availableBal) revert insufficientBalance();
   (bool success, ) = msg.sender.call{value: amount}("");
-  console.log("amount is sent ? ",success);
 }
 
 /// return the current balance of the user (sender)
   function checkBalance() view public returns(uint256){
     uint256 tempBal = balances[msg.sender];
-    console.log("Check balance for ",msg.sender, ": ",tempBal);
+   // console.log("Check balance for ",msg.sender, ": ",tempBal);
     return tempBal; //balances[msg.sender]
   }
 
